4f39de800b27a009590830a0cf3bd1d2a7e281de
Will <willyknapp@gmail.com>
2022-06-06 20:34:45 -0400

    routing-macing integration

--- public/javascript/leaflet.js
+++ public/javascript/leaflet.js
@@ -1,5 +1,4 @@
-var map = L.map('map').setView([40.7125, -74.0433], 14);
-
+var map = L.map('map').setView([40.712587, -74.043421], 18);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -10,6 +9,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1Ijoid2lsbGtheTEwIiwiYSI6ImNsMTN0NmdsYzBjZzAzanAzbHl0c2NzOWQifQ.eIR5fE294dHpU_Ef7b-QFg'
 }).addTo(map);
 
+L.Routing.control({
+  waypoints: [
+    L.latLng(40.712587, -74.043421),
+    L.latLng(39.953773, -75.936127)
+  ],
+  router: L.Routing.mapbox('sk.eyJ1Ijoid2lsbGtheTEwIiwiYSI6ImNsNDNmM3dpcjE3aDUzY29maGNlaDFzMmIifQ.JxwYKZq9mVge1pOMpCcl7Q')
+}).addTo(map);
 
 var popup = L.popup();
 function onMapClick(e) {
---
+++ public/javascript/routes.js
@@ -0,0 +1,10 @@
+let routes = [
+  {
+    start: "Escape Campervan",
+    end: "Lancaster, PA (Victory Brewing)",
+    startLat: 40.712587,
+    startLong: -74.043421,
+    endLat: 39.953773,
+    endLong: -75.936127
+  }
+]
