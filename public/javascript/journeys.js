377f7004e342ff33d58e67fbc7c983731db5dc6b
William Knapp <willyknapp@gmail.com>
2022-06-10 10:28:49 -0400

    waypoints file Written. Needs export

---
+++ public/javascript/journeys.js
@@ -0,0 +1,182 @@
+let waypoints = [
+  {
+    name: "Escape Campervan",
+    lat: 40.712587,
+    long: -74.043421
+  },
+  {
+     name: "Lancaster, PA (Victory Brewing)",
+     long: 39.953773,
+     lat: -75.936127
+  },
+  {
+     name: "Rock and Roll Hall of Fame",
+     long: 41.508501,
+     lat: -81.695468
+  },
+  {
+     name: "First Campsite",
+     long: 41.329197,
+     lat: -82.618303
+  } ,
+  {
+     name: "Chicago (Second City)",
+     long: 41.911595,
+     lat: -87.634882
+  },
+  {
+     name: "Iowa State Fair",
+     long: 41.593407,
+     lat: -9.545237
+  },
+  {
+     name: "Lancaster, PA (Victory Brewing)",
+     long: 39.953773,
+     lat: -75.936127
+  },
+  {
+     name: "Badlands National Park",
+     long: 43.894643,
+     lat: -102.414.36
+  },
+  {
+     name: "Wind Cave National Park",
+     long: 43.556487,
+     lat: -103.478358
+  },
+  {
+     name: "Yellowstone National Park",
+     long: 44.282029,
+     lat: -110.626048
+  },
+  {
+     name: "Grand Teton National Park",
+     long: 43.909130,
+     lat: -110.642763
+  },
+  {
+     name: "Craters of the Moon",
+     long: 43.460717,
+     lat: -113.556919
+  },
+  {
+     name: "Great Basin National Park",
+     long: 38.990022,
+     lat: -114.222692
+  },
+  {
+     name: "Napa Valley",
+     long: 38.484016,
+     lat: -122.157621
+  },
+  {
+     name: "San Francisco",
+     long: 37.806370,
+     lat: -122.157621
+  },
+  {
+     name: "Yosemite",
+     long: 37.748233,
+     lat: -119.586321
+  },
+  {
+     name: "Kings Canyon",
+     long: 36.791950,
+     lat: -118.679429
+  },
+  {
+     name: "Los Angeles",
+     long: 34.072650,
+     lat: -118.292386
+  },
+  {
+     name: "Disneyland",
+     long: 33.809079,
+     lat: -117.918958
+  },
+  {
+     name: "Joshua Tree National Park",
+     long: 34.093619,
+     lat: -116.160834
+  },
+  {
+     name: "Phoenix Campsite",
+     long: 33.858587,
+     lat: -112.304446
+  },
+  {
+     name: "Musical Instrument Museum",
+     long: 33.678210,
+     lat: -111.978589
+  },
+  {
+     name: "Grand Canyon National Park",
+     long: 36.049217,
+     lat: -112.119395
+  },
+  {
+     name: "Glen Canyon Campsite",
+     long: 36.859737,
+     lat: -75.936127
+  },
+  {
+     name: "Horseshoe Bend",
+     long: 36.880073,
+     lat: -111.510437
+  },
+  {
+     name: "Petroglyph National Monument (Albuquerque)",
+     long: 35.138846,
+     lat: -106.711289
+  },
+  {
+     name: "OKC Alpaca Camp",
+     long: 35.268246,
+     lat: -97.761267
+  },
+  {
+     name: "Oklahoma City",
+     long: 35.452582,
+     lat: -97.531600
+  },
+  {
+     name: "Hot Springs National Park",
+     long: 34.517234,
+     lat: -93.046217
+  },
+  {
+     name: "Memphis",
+     long: 35.115736,
+     lat: -90.031178
+  },
+  {
+     name: "Nashville",
+     long: 36.206854,
+     lat: -86.691860
+  },
+  {
+     name: "Shenandoah National Park",
+     long: 38.527733,
+     lat: -78.438081
+  },
+  {
+     name: "Lancaster, PA (Victory Brewing)",
+     long: 39.953773,
+     lat: -75.936127
+  },
+  {
+     name: "Washington D.C.",
+     long: 38.905309,
+     lat: -77.057344
+  },
+  {
+     name: "Baltimore",
+     long: 39.265398,
+     lat: -76.579776
+  },
+  {
+     name: "Philadelphia",
+     long: 39.968682,
+     lat: -75.178242
+  }
+]
